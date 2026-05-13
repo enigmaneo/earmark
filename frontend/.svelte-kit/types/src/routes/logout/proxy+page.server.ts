58edfd9c8ad7aee090b340396ac27f80c9a4d121
rmontgomery2018 <rmontgo@gmail.com>
2026-05-12 23:12:18 -0400

    untrack f.svelte-kit

--- frontend/.svelte-kit/types/src/routes/logout/proxy+page.server.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load = async ({ cookies }: Parameters<PageServerLoad>[0]) => {
-	cookies.delete('earmark_session', { path: '/' });
-	redirect(302, '/login');
-};
